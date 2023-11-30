eed3bb9f91777db817d833b0cc395a11607bd29e
lnachman <lnachman@akamai.com>
2023-11-30 09:22:18 +0200

    fix: add redis client

--- server/interfaces/interfaces.py
+++ server/interfaces/interfaces.py
@@ -2,13 +2,20 @@ from fastapi import Query
 from pydantic import BaseModel, Field
 
 
-class CreateDocumentPayload(BaseModel):
+class Document(BaseModel):
+    title: str
+    content: str
+    dist: float
+
+
+class CreateDocumentPayload(Document, BaseModel):
     title: str = Field(..., example="Document title")
     content: str = Field(..., example="Document content")
 
 
 class SearchQuery(BaseModel):
     text: str = Query(..., example="sushi")
+
 
 class SearchResponse(BaseModel):
     ...
