421f6884ff62fc1b814b9fd086ebefcce9e6242a
Omer Cohen <omercotkd@gmail.com>
2023-11-30 18:28:23 +0200

    save documents in redis in post documents

--- server/routers/__init__.py
+++ server/routers/__init__.py
@@ -9,37 +9,40 @@ from db.models import Document
 router = APIRouter()
 redis_embedding_client = RedisStorageVector(redis_uri=ENV_VARIABLES.REDIS_URI)
 
+
 ## Will use it for many and one document as well - same process just one element [doc]
 @router.post("/document/bulk")
-def create_documents(
-    request: Request,
-    payload: list[CreateDocumentPayload]
-):
-    # TODO create documents
+def create_documents(request: Request, payload: list[CreateDocumentPayload]):
 
     ## save on mongodb
+    for item in payload:
+        Document(
+            extrnal_id=None,
+            title=item.title,
+            content=item.content,
+            contact=None,
+            link=None,
+        ).save()
 
     ## save on redis
     documents = [item.model_dump() for item in payload]
 
-    print(documents)
     redis_embedding_client.load(documents)
 
     return {"message": "create documents"}
 
+
 @router.get("/search")
 def search_documents(request: Request, query: SearchQuery = Depends()):
-    # TODO search documents
-    print(query.text)
-    docs = redis_embedding_client.find_similarity_documents(question=query.text, threshold=query.threshold)
-    print(docs)
+    docs = redis_embedding_client.find_similarity_documents(
+        question=query.text, threshold=query.threshold
+    )
 
     return {"docs": docs}
 
 
 @router.put("/load")
 def load_documents_to_redis():
-
     documents = Document.find()
 
     redis_embedding_client.load([item.model_dump() for item in documents])
